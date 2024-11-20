ffb457ccb225f63c7eefe6a5e9d12e1bd0bee51f
sheep <pierre.coric.lg@gmail.com>
2024-11-20 15:10:10 -0500

    it works

--- 2516_take_k.cpp
+++ 2516_take_k.cpp
@@ -1,146 +1,43 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
+
         if(k == 0) {
-            return 0;
+            return k;
         }
-        //The max depth in which we have to scan
-        int maxDepth = 0;
-        int min = k * 3;
-        int smallest;
-        bool isOdd;
-        if(s.size() % 2 != 0) {
-            isOdd = true;
+        
+        //Array to count the letters.
+        int totalLetters [3] = {0 ,0, 0};
+        int size = s.size();
+        int result = size;
+        
+        //Count the total of each letters.
+        for(auto &c : s) {
+            totalLetters[c - 'a'] ++;
         }
-
-        //Variables to count the letters.
-        int A(0), B(0), C(0);
-
-        //Iterate over the string once to make sure that the problem is solvable.
-        //From both side
-        for(int i = 0; i < s.size() / 2; i++) {
-            //Count from both sides
-            countAdd(s[i], A, B, C);
-            countAdd(s[s.size() - i - 1], A, B, C);
-            //if we found that the problem is solvable we move on.
-            if(checkCount(A, B, C, k)) {
-                maxDepth = A + B + C;
-                break;
-            }
-        }
-        //if there is still a value in the middle.
-        if(s.size() % 2 != 0) {
-            if (maxDepth == 0) {
-                countAdd(s[s.size() / 2], A, B, C);
-                if(checkCount(A, B, C, k)) {
-                    maxDepth = A + B + C;
-                }
-            }
-        }
-
-        //If it is impossible to solve
-        if(maxDepth == 0) {
+            
+        //Return -1 if the count is not reachable.
+        if(totalLetters[0] < k || totalLetters[1] < k || totalLetters[2] < k) {
             return -1;
         }
 
-        //If it is possible
+        //If there are enough letters to solve the problem
         else {
-            //The worst case is that the max Depth is the solution
-            smallest = maxDepth;
-
-            int L(0), R(0);
-            A = 0;
-            B = 0;
-            C = 0;
-            //move left and right to the max depth.
-            while(L < maxDepth / 2) {
-                countAdd(s[L], A, B, C);
-                countAdd(s[s.size() - R - 1], A, B, C);
-                if(checkCount(A, B, C, k)) {
-                    if(A + B + C < smallest) {
-                        smallest = A + B + C;
-                    }
+            //Sliding window
+            int l = 0;
+            for(int r = 0; r < size; r++) {
+                totalLetters[s[r] - 'a'] --;
+                while(totalLetters[0] < k || totalLetters[1] < k || totalLetters[2] < k) {
+                    totalLetters[s[l] - 'a'] ++;
+                    l ++;
                 }
-                cout << "smallest " << smallest << " L: " << L << " R: " << s.size() - R - 1 << "    ABC  " << A <<  B << C << endl;
-                L++;
-                R++;
+                //Check the size of the result.
+                if (result > (size - (r - l + 1))) {
+                    result = size - (r - l + 1);
+                }
             }
-            cout << endl;
-
-            R -= 2;
-            //Move each cursor separetadeldelady
-            //Left goes forward
-            while(L < maxDepth) {
-                countAdd(s[L], A, B, C);
-                if( R > 0) {
-                    countDelete(s[s.size() - R - 1], A, B, C);
-                }
-                if(checkCount(A, B, C, k)) {
-                    if(A + B + C < smallest) {
-                        smallest = A + B + C;
-                    }
-                }
-                cout << "smallest " << smallest << " L: " << L << " R: " << s.size() - R - 1 << "    ABC  " << A <<  B << C << endl;
-                L++;
-                R--;
-            }
-
-            cout << endl;
-
-            //Back to center
-            L = maxDepth / 2;
-            R = maxDepth / 2;
-            cout << "smallest " << smallest << " L: " << L << " R: " << s.size() - R - 1 << "    ABC  " << A <<  B << C << endl;
-            cout << endl;
-
-            //Right goes deeper
-            while(R < maxDepth) {
-                countDelete(s[L], A, B, C);
-                if( R > 0) {
-                    countAdd(s[s.size() - R - 1], A, B, C);
-                }
-                if(checkCount(A, B, C, k)) {
-                    if(A + B + C < smallest) {
-                        smallest = A + B + C;
-                    }
-                }
-                cout << "smallest " << smallest << " L: " << L << " R: " << s.size() - R - 1 << "    ABC  " << A <<  B << C << endl;
-                L--;
-                R++;
-            }
-            
-        return smallest;
         }
-    }
-    //Add value when we go deeper into the string
-    void countAdd(char c, int &A, int &B, int &C) {
-        if(c == 'a') {
-            A++;
-        }
-        else if(c == 'b') {
-            B++;
-        }
-        else if(c == 'c') {
-            C++;
-        }
-    }
-
-    //Retrieve value when we move back from the middle.
-    void countDelete(char c, int &A, int &B, int &C) {
-        if(c == 'a') {
-            A--;
-        }
-        else if(c == 'b') {
-            B--;
-        }
-        else if(c == 'c') {
-            C--;
-        }
-    }
-
-    bool checkCount(int A, int B, int C, int k) {
-        if(A >= k && B >= k && C >= k) {
-            return true;
-        } else return false;
+        //Return the result;
+        return result;
     }
 };
